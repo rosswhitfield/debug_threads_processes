109d220acd69bdce6868c64756e5c71a434005ad
Ross Whitfield <whitfieldre@ornl.gov>
2022-09-22 09:49:38 -0400

    post

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 from multiprocessing import Process, set_start_method
-from urllib import request
+from urllib import request, error
+import json
 
 if sys.platform == 'darwin':
     try:
@@ -9,11 +10,23 @@ if sys.platform == 'darwin':
         # context can only be set once
         pass
 
-def send():
-    with request.urlopen('http://www.python.org/') as f:
-        print(f.read(300))
 
-        
+def send():
+    msgs = {"hello": "world"}
+    try:
+        req = request.Request("http://localhost",
+                              data=json.dumps(msgs).encode(),
+                              headers={'Content-Type': 'application/json'},
+                              method='POST')
+        resp = request.urlopen(req)
+    except error.HTTPError as e:
+        print(e.code, e.read())
+    except error.URLError as e:
+        print(e)
+    else:
+        print(resp.status, resp.read())
+
+
 process = Process(target=send)
 process.start()
 process.join()
