0fcdd9d698209f2906a7b8fbd1ff05f8aa7e8b0c
Ross Whitfield <whitfieldre@ornl.gov>
2022-09-22 10:54:29 -0400

    Revert "simplify"
    This reverts commit f570a9e3052ebace7d9bdd90fce05fca96d690dd.

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+import sys
 from multiprocessing import Process, set_start_method
 from threading import Thread
+from urllib import request, error
+import json
 import time
 
-set_start_method('fork')
+if sys.platform == 'darwin':
+    try:
+        set_start_method('fork')
+    except RuntimeError:
+        # context can only be set once
+        pass
 
 def sleep():
     print("Start sleep")
@@ -10,7 +18,19 @@ def sleep():
     print("End sleep")
 
 def send():
-    print("Hello world")
+    msgs = {"hello": "world"}
+    try:
+        req = request.Request("http://localhost",
+                              data=json.dumps(msgs).encode(),
+                              headers={'Content-Type': 'application/json'},
+                              method='POST')
+        resp = request.urlopen(req)
+    except error.HTTPError as e:
+        print(e.code, e.read())
+    except error.URLError as e:
+        print(e)
+    else:
+        print(resp.status, resp.read())
 
 
 t = Thread(target=sleep)
